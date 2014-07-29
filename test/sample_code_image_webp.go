c354a3eeafcf70dc83b87ca6e6c00131a36f73a1
Kamol Mavlonov <kamol@701search.com>
2014-07-29 12:21:42 +0800

    Added sample code how to convert from image to webp

---
+++ test/sample_code_image_webp.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"os"
+
+	"github.com/chai2010/webp"
+)
+
+func main() {
+
+	i, o := os.Args[1], os.Args[2]
+
+	f, err := os.Open(i)
+	defer f.Close()
+	if err != nil {
+		log.Fatal("Error unable to open a file: ", err)
+	}
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatal("Error unable to decode a file: ", err)
+	}
+
+	w, err := os.Create(o)
+	defer w.Close()
+	if err != nil {
+		log.Fatal("Error unable to create a file: ", err)
+	}
+
+	webp.Encode(w, img, &webp.Options{true, 90})
+}
