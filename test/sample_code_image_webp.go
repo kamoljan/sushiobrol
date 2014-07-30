3e33ad39ec76e3fac334fecd6fee82422d17091f
Kamol Mavlonov <kamol@701search.com>
2014-07-30 17:58:36 +0800

    Code refactoring

--- test/sample_code_image_webp.go
+++ test/sample_code_image_webp.go
@@ -1,35 +1,32 @@
 package main
 
 import (
+	"github.com/chai2010/webp"
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
 	"os"
-
-	"github.com/chai2010/webp"
 )
 
 func main() {
-
 	i, o := os.Args[1], os.Args[2]
-
 	f, err := os.Open(i)
-	defer f.Close()
 	if err != nil {
 		log.Fatal("Error unable to open a file: ", err)
 	}
-
+	defer f.Close()
 	img, _, err := image.Decode(f)
 	if err != nil {
 		log.Fatal("Error unable to decode a file: ", err)
 	}
-
 	w, err := os.Create(o)
-	defer w.Close()
 	if err != nil {
 		log.Fatal("Error unable to create a file: ", err)
 	}
-
-	webp.Encode(w, img, &webp.Options{true, 90})
+	defer w.Close()
+	err = webp.Encode(w, img, &webp.Options{true, 90})
+	if err != nil {
+		log.Fatal("Error unable to encode a file: ", err)
+	}
 }
